d365fbde89e949274f34aa25beae05dc467c6775
Neglolz <johribe@gmail.com>
2017-10-15 01:06:27 +0200

    ajout createTerrain

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,53 +1,104 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+
+    <div class="terrain">
+      {{terrain}}
+      <div v-for="Cell in terrain">
+        o
+      </div>
+    </div>
+    <button @click="shoot">Tirer</button>
+    <button @click="pass('he')">Passer</button>
+    <button @click="test">JSON</button>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  export default {
+    name: 'hello',
+    props:{
+
+    },
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+        wTerrain: 2,
+        hTerrain: 3,
+        terrain: Object
+      }
+    },
+    methods: {
+      shoot(){
+        console.log('shoot')
+      },
+      pass(cible){
+        cible ? console.log(cible) : console.log('choisir une cible');
+      },
+      dribble(){
+        console.log('ballon.pos +1')
+      },
+      // generation du json terrain. Prend en parametre un strin position du ballon
+      // un tableau de string position des joueurs du user1 + user2
+      createTerrain( posBallon){
+          // , posJoueursUser1, posjoueursUser2
+          let jsonData = '{';
+          for( let x = 1 ; x <= this.wTerrain ; x++ ){
+            for( let y = 1 ; y <= this.hTerrain ; y++ ){
+              //console.log(x,y);
+              jsonData += '{' ;
+              jsonData += '"name": "' + x.toString()+y.toString() +'",';
+              (posBallon.toString() === x.toString()+y.toString()) ? jsonData+='"isBallon":"true"' : jsonData+='"isBallon":"false"' ;
+
+
+              (y === this.hTerrain  && x === this.wTerrain) ? jsonData+='}' : jsonData+='},' ;
+            }
+          }
+          jsonData += '}'
+          console.log(JSON.parse(JSON.stringify(jsonData)))
+          jsonData = JSON.parse(JSON.stringify(jsonData))
+          this.terrain= jsonData
+         // return JSON.parse(JSON.stringify(jsonData))
+      },
+      test(value){
+        this.createTerrain('12')
+        //console.log(this.terrain)
+      }
+    },
+    mounted(){
+        this.terrain = this.createTerrain('12')
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 
-a {
-  color: #42b983;
-}
+  a {
+    color: #42b983;
+  }
+
+  .cell {
+    display: block;
+    width: 50px;
+    float: left;
+    border: 1px solid black;
+  }
+
+  .terrain {
+    width: 450px;
+  }
 </style>
